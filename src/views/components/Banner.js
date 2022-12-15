b88f8ba1bcb475804a8940dfc9fe0454af0b3c44
“T3sultan” <incognito.soul011@gmail.com>
2022-12-15 16:14:08 +0600

    carosul custom

--- src/views/components/Banner.js
+++ src/views/components/Banner.js
@@ -11,9 +11,17 @@ const Banner = () => {
       <div id="slide1" className="carousel-item relative w-full">
         <img src={image1} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
+          <div className=" ml-20">
+            <h3 className="text-white text-xl font-bold ">BEST OF 2022</h3>
+            <h1 className="text-white text-3xl font-bold mt-4">
+              'IMMORTAL X-MEN': IGN'S BEST <br /> COMIC BOOK SERIES OF THE YEAR
+            </h1>
+            <div>
+              <button className="text-white font-bold border-2 w-28 mt-4 rounded-tl-xl rounded-br-xl h-14">
+                FIRST LOOK!
+              </button>
+            </div>
+          </div>
           <a href="#slide2" className="btn btn-circle">
             ❯
           </a>
@@ -22,9 +30,17 @@ const Banner = () => {
       <div id="slide2" className="carousel-item relative w-full">
         <img src={image2} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
+          <div className=" ml-20">
+            <h3 className="text-white text-xl font-bold ">BEST OF 2022</h3>
+            <h1 className="text-white text-3xl font-bold mt-4">
+              'IMMORTAL X-MEN': IGN'S BEST <br /> COMIC BOOK SERIES OF THE YEAR
+            </h1>
+            <div>
+              <button className="text-white font-bold border-2 w-28 mt-4 rounded-tl-xl rounded-br-xl h-14">
+                FIRST LOOK!
+              </button>
+            </div>
+          </div>
           <a href="#slide3" className="btn btn-circle">
             ❯
           </a>
@@ -33,9 +49,17 @@ const Banner = () => {
       <div id="slide3" className="carousel-item relative w-full">
         <img src={image3} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
+          <div className=" ml-20">
+            <h3 className="text-white text-xl font-bold ">BEST OF 2022</h3>
+            <h1 className="text-white text-3xl font-bold mt-4">
+              'IMMORTAL X-MEN': IGN'S BEST <br /> COMIC BOOK SERIES OF THE YEAR
+            </h1>
+            <div>
+              <button className="text-white font-bold border-2 w-28 mt-4 rounded-tl-xl rounded-br-xl h-14">
+                FIRST LOOK!
+              </button>
+            </div>
+          </div>
           <a href="#slide4" className="btn btn-circle">
             ❯
           </a>
@@ -44,9 +68,36 @@ const Banner = () => {
       <div id="slide4" className="carousel-item relative w-full">
         <img src={image4} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
+          <div className=" ml-20">
+            <h3 className="text-white text-xl font-bold ">BEST OF 2022</h3>
+            <h1 className="text-white text-3xl font-bold mt-4">
+              'IMMORTAL X-MEN': IGN'S BEST <br /> COMIC BOOK SERIES OF THE YEAR
+            </h1>
+            <div>
+              <button className="text-white font-bold border-2 w-28 mt-4 rounded-tl-xl rounded-br-xl h-14">
+                FIRST LOOK!
+              </button>
+            </div>
+          </div>
+          <a href="#slide5" className="btn btn-circle">
+            ❯
           </a>
+        </div>
+      </div>
+      <div id="slide5" className="carousel-item relative w-full">
+        <img src={image5} className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <div className=" ml-20">
+            <h3 className="text-white text-xl font-bold ">BEST OF 2022</h3>
+            <h1 className="text-white text-3xl font-bold mt-4">
+              'IMMORTAL X-MEN': IGN'S BEST <br /> COMIC BOOK SERIES OF THE YEAR
+            </h1>
+            <div>
+              <button className="text-white font-bold border-2 w-28 mt-4 rounded-tl-xl rounded-br-xl h-14">
+                FIRST LOOK!
+              </button>
+            </div>
+          </div>
           <a href="#slide1" className="btn btn-circle">
             ❯
           </a>
