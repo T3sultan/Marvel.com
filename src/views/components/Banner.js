0780beea1840e589cc26a1da9e4396b92a2420ed
“T3sultan” <incognito.soul011@gmail.com>
2022-12-15 15:51:46 +0600

    footer and carosul

---
+++ src/views/components/Banner.js
@@ -0,0 +1,59 @@
+import React from "react";
+import image1 from "../../assets/cpatainama.1db6e6722978078a3d77.jpg";
+import image2 from "../../assets/638edc9e2b9fa.53af284cb8121f883fa0.jpg";
+import image3 from "../../assets/639970321781d.243d6af97725006961bf.jpg";
+import image4 from "../../assets/villern.81924fbad0bd8f0ca0af.jpg";
+import image5 from "../../assets/redwwomen.6b2ba77813cbbf0dc20c.jpg";
+
+const Banner = () => {
+  return (
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src={image1} className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src={image2} className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src={image3} className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img src={image4} className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/views/components/Footer.js
@@ -0,0 +1,91 @@
+import React from "react";
+import mainlogo from "../../assets/logomarvel.png";
+import picture1 from "../../assets/picture1.png";
+import picture2 from "../../assets/picture2.png";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer p-10 bg-black text-base-content">
+        <div>
+          <img className="w-28" src={mainlogo} alt="" />
+        </div>
+        <div>
+          <span className=" text-white font-bold ">ABOUT MARVEL</span>
+          <a className="link text-white font-bold link-hover">HELP/FAQS</a>
+          <a className="link text-white font-bold link-hover">CAREERS</a>
+          <a className="link text-white font-bold link-hover">INTERNSHIPS</a>
+        </div>
+        <div>
+          <span className=" text-white font-bold ">ADVERTISING</span>
+          <a className="link text-white font-bold link-hover">DISNEY+</a>
+          <a className="link text-white font-bold link-hover">MARVELHQ.COM</a>
+          <a className="link text-white font-bold link-hover">
+            REDEEM DIGITAL COMICS
+          </a>
+        </div>
+        <div className="">
+          <div className="flex items-center gap-8">
+            <img className="w-16" src={picture1} alt="" />
+            <div>
+              <h1 className="text-white">MARVEL INSIDER</h1>
+              <p className="text-gray-200">
+                Get Rewarded for Being a Marvel Fan
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-8">
+            <img className="w-16" src={picture2} alt="" />
+            <div>
+              <h1 className="text-white">MARVEL UNLIMITED</h1>
+              <p className="text-gray-200">
+                Access Over 30,000+ Digital Comics
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <span className=" text-white font-bold">FOLLOW MARVEL</span>
+          <div className="grid grid-flow-col gap-6 mt-4">
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current bg-white rounded-full cursor-pointer"
+              >
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current bg-white rounded-full cursor-pointer"
+              >
+                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current bg-white rounded-full cursor-pointer"
+              >
+                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
